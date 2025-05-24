a940c47f4c8a42053d980cedebb3c06f247d43e6
shivam <shiva147m@gmail.com>
2025-05-24 17:10:25 +0530

    sidebar

--- Assignment/src/components/Sidebar/Sidebar.jsx
+++ Assignment/src/components/Sidebar/Sidebar.jsx
@@ -11,9 +11,9 @@ const Sidebar = () => {
     return (
         <div className="sidebar">
             <div class="general">
-                <h2>General</h2>
+                <h3>General</h3>
                 <ul>
-                    <li> <MdDashboardCustomize /> <a href=''>Dashboard</a> </li>
+                    <li className="dashboard"> <MdDashboardCustomize /> <a href='' style={{ color: "#3734A9" }}>Dashboard</a> </li>
                     <li> <RiArrowUpDownFill /> <a href=''>History</a> </li>
                     <li> <FaCalendarAlt /> <a href=''>Calendar</a> </li>
                     <li> <FaSquarePlus /> <a href=''>Appointments</a> </li>
@@ -21,10 +21,10 @@ const Sidebar = () => {
                 </ul>
             </div>
             <div className="Tools">
-                <h2>Tools</h2>
+                <h3>Tools</h3>
                 <ul>
                     <li> <IoChatbubbleEllipsesSharp /> <a href=''>Chat</a> </li>
-                    <li> <IoIosCall /> <a href=''>Call</a> </li>
+                    <li> <IoIosCall /> <a href=''>Support</a> </li>
                 </ul>
             </div>
             <div className="settings">
